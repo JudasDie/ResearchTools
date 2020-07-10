e833ac398399467cca8d030e023889017c2659ea
“JudasDie” <zpzhang@1995@gmail.com>
2020-07-10 17:21:28 +0800

    fix merge bugs

--- analysz/merge_videos.py
+++ analysz/merge_videos.py
@@ -6,6 +6,8 @@ except:
     from moviepy.editor import *
 
 video_paths = ['./vis/demo1.mp4', './vis/demo2.mp4', './vis/demo3.mp4']  # in-order
+temp = [VideoFileClip(i) for i in video_paths]
+
 print('[*] ====> merge videos <=====')
-final_clip = concatenate_videoclips(video_paths)
+final_clip = concatenate_videoclips(temp)
 final_clip.to_videofile("./target.mp4", fps=20, remove_temp=False)
